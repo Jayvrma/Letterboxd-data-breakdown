7d2ba88434ff4394fecc7fbfc4e416c888334c6b
Wi1Ian <81652539+Wi1Ian@users.noreply.github.com>
2026-06-05 14:09:16 -0400

    First step
    I wrote a basic python program that uses the letterboxd csv data, and converts each movie into a dataclass. And puts them all in a list.

---
+++ watched.py
@@ -0,0 +1,23 @@
+import csv 
+from dataclasses import dataclass
+@dataclass
+class Movie:
+    date:str
+    name:str
+    year:int
+    url:str
+
+watched = "watched.csv"
+rows = []
+movies= []
+
+with open(watched, 'r') as csvfile:
+    csvReader = csv.reader(csvfile)
+    fields = next(csvReader)
+    for row in csvReader:
+        rows.append(row)
+for row in rows:
+    movies.append(Movie(row[0],row[1],row[2],row[3]))
+
+for movie in movies:
+    print(movie)
